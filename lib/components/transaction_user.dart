ed6ed3995feccbe6873d20955a8bd9dca85ea14d
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2021-12-29 16:36:31 -0300

    Tornando itens da classe Transaction em widgets

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:minhas_despesas/components/transaction_form.dart';
+import 'package:minhas_despesas/components/transaction_list.dart';
+import 'package:minhas_despesas/models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 300.10,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Novo celular',
+      value: 999.80,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(transactions: _transaction),
+        TransactionForm(),
+      ],
+    );
+  }
+}
--- lib/despesas.dart
+++ lib/despesas.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:minhas_despesas/components/transaction_list.dart';
-
-import 'package:minhas_despesas/models/transaction.dart';
+import 'package:minhas_despesas/components/transaction_user.dart';
 
 class Despesas extends StatefulWidget {
   const Despesas({Key? key}) : super(key: key);
@@ -13,24 +11,6 @@ class Despesas extends StatefulWidget {
 }
 
 class _DespesasState extends State<Despesas> {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-  //Alimntando a classe Transaction 
-  final _transaction = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis de corrida',
-      value: 300.10,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Novo celular',
-      value: 999.00,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,39 +33,7 @@ class _DespesasState extends State<Despesas> {
                 ),
               ),
             ),
-            //Chamando a classe e passando o parâmetro nomeado
-            TransactionList(transactions: _transaction),
-            Card(
-              elevation: 7,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: titleController,
-                      decoration:
-                          InputDecoration(labelText: 'Nome do produto:'),
-                    ),
-                    TextField(
-                      controller: valueController,
-                      decoration: InputDecoration(labelText: 'Valor (R\$):'),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(360, 10, 10, 0),
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.purple)),
-                          onPressed: () {
-                            print(titleController.text);
-                            print(valueController.text);
-                          },
-                          child: Icon(Icons.add)),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            TransactionUser(),
           ],
         ));
   }
