3e112489121ff4955b68e26be68e1f418f891262
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2021-12-29 00:55:48 -0300

    Data formatada com o package intl

--- lib/despesas.dart
+++ lib/despesas.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:minhas_despesas/models/transaction.dart';
 
 class Despesas extends StatefulWidget {
@@ -85,7 +86,7 @@ class _DespesasState extends State<Despesas> {
                                   fontSize: 16, fontWeight: FontWeight.bold),
                             ),
                             //Data da compra
-                            Text(tr.date.toString(),
+                            Text(DateFormat('MMM d, y').format(tr.date),
                                 style: TextStyle(color: Colors.grey)),
                           ],
                         )
