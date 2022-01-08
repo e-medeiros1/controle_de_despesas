7bff821d40d0b90d648ef750ce6a18162fbb10bc
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-08 17:51:16 -0300

    Lógica do Gráfico(Chart) e implementação na tela

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,19 +1,50 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:minhas_despesas/components/chat_bar.dart';
+import 'package:minhas_despesas/models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key? key}) : super(key: key);
+  const Chart({Key? key, required this.recentTransactions}) : super(key: key);
+
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, dynamic>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        bool sameDay = recentTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentTransactions[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
+    return Card(
       elevation: 7,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [
-          
-        ],
+        children: groupedTransactions.map((tr) {
+          return ChatBar(
+            label: tr['day'].toString(),
+            value: double.parse(tr['value'].toString()),
+            percentage: 0,
+          );
+        }).toList(),
       ),
     );
   }
